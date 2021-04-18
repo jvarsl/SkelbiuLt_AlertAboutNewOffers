3440f575fb9fb528aff5dd72ee9fa41157d021e6
jvarsl <j.varslauskas@gmail.com>
2021-04-18 22:31:11 +0300

    Reworked (extra separation for methods, added async for responsiveness)

--- SkelbiuLt_AlertAboutNewOffers/Program.cs
+++ SkelbiuLt_AlertAboutNewOffers/Program.cs
@@ -6,84 +6,104 @@ using System.Net.Mail;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace SkelbiuLt_AlertAboutNewOffers
 {
     class Program
     {
-        public static Dictionary<string, string> ProductLinkDictionary = new Dictionary<string, string>()   {
-            {"Logitech29","https://www.skelbiu.lt/skelbimai/?autocompleted=1&keywords=logitech+g29&submit_bn=&cities=465&distance=0&mainCity=1&search=1&category_id=0&type=0&user_type=0&ad_since_min=0&ad_since_max=0&visited_page=1&orderBy=1&detailsSearch=0" },
-            {"Logitech920","https://www.skelbiu.lt/skelbimai/?autocompleted=1&keywords=logitech+g920&submit_bn=&cities=465&distance=0&mainCity=1&search=1&category_id=0&type=0&user_type=0&ad_since_min=0&ad_since_max=0&visited_page=1&orderBy=1&detailsSearch=0" },
-            {"Logitech27","https://www.skelbiu.lt/skelbimai/?autocompleted=1&keywords=logitech+g27&submit_bn=&cities=465&distance=0&mainCity=1&search=1&category_id=0&type=0&user_type=0&ad_since_min=0&ad_since_max=0&visited_page=1&orderBy=1&detailsSearch=0" },
-            {"Thrustmaster","https://www.skelbiu.lt/skelbimai/?autocompleted=1&keywords=thrustmaster&submit_bn=&cities=465&distance=0&mainCity=1&search=1&category_id=0&type=0&user_type=0&ad_since_min=0&ad_since_max=0&visited_page=1&orderBy=1&detailsSearch=0" }
+        public static Dictionary<string, string> ProductLinkDictionary = new Dictionary<string, string>()
+        {
+            {"Logitech29", "https://www.skelbiu.lt/skelbimai/?autocompleted=1&keywords=logitech+g29&submit_bn=&cities=465&distance=0&mainCity=1&search=1&category_id=0&type=0&user_type=0&ad_since_min=0&ad_since_max=0&visited_page=1&orderBy=1&detailsSearch=0" },
+            {"Logitech920", "https://www.skelbiu.lt/skelbimai/?autocompleted=1&keywords=logitech+g920&submit_bn=&cities=465&distance=0&mainCity=1&search=1&category_id=0&type=0&user_type=0&ad_since_min=0&ad_since_max=0&visited_page=1&orderBy=1&detailsSearch=0" },
+            {"Logitech27", "https://www.skelbiu.lt/skelbimai/?autocompleted=1&keywords=logitech+g27&submit_bn=&cities=465&distance=0&mainCity=1&search=1&category_id=0&type=0&user_type=0&ad_since_min=0&ad_since_max=0&visited_page=1&orderBy=1&detailsSearch=0" },
+            {"Thrustmaster", "https://www.skelbiu.lt/skelbimai/?autocompleted=1&keywords=thrustmaster&submit_bn=&cities=465&distance=0&mainCity=1&search=1&category_id=0&type=0&user_type=0&ad_since_min=0&ad_since_max=0&visited_page=1&orderBy=1&detailsSearch=0" }
         };
 
-        public static Dictionary<string, int> ProductCountDictionary = new Dictionary<string, int>()   {
-            {"Logitech29",0 },
-            {"Logitech920",0 },
-            {"Logitech27",0 },
-            {"Thrustmaster",0 }
-        };
+        public static Dictionary<string, int> ProductCountDictionary = new Dictionary<string, int>();
 
         public static ConsoleColor OriginalConsoleColor = Console.BackgroundColor;
-        static void Main(string[] args)
-        {
-            string regexPattern = @"Skelbim.:\s(\d+)";
+        public static bool NewOfferFound = false;
+        public static string MailContentLinks = string.Empty;
 
-            //infinite loop works until script is killed
+        static async Task Main()
+        {
+            ProductCountDictionary = ProductLinkDictionary.ToDictionary(entry => entry.Key, entry => 0);
+
+            //infinite loop works until console is closed
             while (true)
             {
-                ExtractData(regexPattern);
-                Console.Write($"Ran on {DateTime.Now} - ");
-                foreach (var item in ProductCountDictionary)
-                {
-                    Console.Write($"{item.Key}({item.Value}); ");
-                }
+                await ExtractDataAsync();
+                if (NewOfferFound) { MakeCuesAsync(MailContentLinks).ConfigureAwait(false); }
 
-                if (Console.BackgroundColor == ConsoleColor.Red) { Console.BackgroundColor = OriginalConsoleColor; }
-                Console.WriteLine();
+                Console.WriteLine(ReturnCurrentProductCount());
+
+                ResetReportingValues();
                 Thread.Sleep(65000);
             }
         }
 
-        static void ExtractData(string regexPattern)
+        static void ResetReportingValues()
         {
-            using (var client = new WebClient())
+            if (NewOfferFound)
             {
-                bool newOfferFound = false;
-                string emailContentLinks = "";
+                NewOfferFound = false;
+                MailContentLinks = string.Empty;
+                Console.BackgroundColor = OriginalConsoleColor;
+            }
+        }
+
+        static string ReturnCurrentProductCount()
+        {
+            string consoleOutput = $"Ran on {DateTime.Now} - ";
+            foreach (var item in ProductCountDictionary)
+            {
+                consoleOutput += $"{item.Key}({item.Value}); ";
+            }
+            return consoleOutput;
+        }
+
+        static async Task ExtractDataAsync()
+        {
+            string regexPattern = @"Skelbim.:\s(\d+)";
+            //@"<div id=\\""adsNumberFilterBar\\"">.*?(\d+).*?<\/div>"; // doesn't work for some reason but works when html is read from txt file
+
+            using (var client = new HttpClient())
+            {
                 foreach (var entry in ProductLinkDictionary)
                 {
-                    string htmlPageResponse = client.DownloadString(entry.Value);
+                    string htmlPageResponse = await client.GetStringAsync(entry.Value);
+
                     int numberOfProductAdverts = ReturnNumberOfProductAdverts(htmlPageResponse, regexPattern);
 
-                    int previousRecorderNumberOfProductAdverts = ProductCountDictionary[entry.Key];
-
-                    //if increase is detected then it will inform the user
-                    if (numberOfProductAdverts > previousRecorderNumberOfProductAdverts && previousRecorderNumberOfProductAdverts != 0)
-                    {
-                        newOfferFound = true;
-                        emailContentLinks += $"{entry.Value}\n";
-                    }
-                    //update offer count
-                    ProductCountDictionary[entry.Key] = numberOfProductAdverts;
-                }
-                //produce sound and send email to get phone notiflication
-                if (newOfferFound)
-                {
-                    MakeCue(emailContentLinks);
+                    CheckProductCountChange(entry, numberOfProductAdverts);
                 }
             }
         }
 
-        static void MakeCue(string emailContentLinks)
+        static void CheckProductCountChange(KeyValuePair<string, string> product, int numberOfProductAdverts)
         {
-            //visual cue
+            int previousRecorderNumberOfProductAdverts = ProductCountDictionary[product.Key];
+
+            //if increase is detected then it will inform the user
+            if (numberOfProductAdverts > previousRecorderNumberOfProductAdverts && previousRecorderNumberOfProductAdverts != 0)
+            {
+                NewOfferFound = true;
+                MailContentLinks += $"<a href='{product.Value}'>{product.Key}</a><br>";
+            }
+            //update product count
+            ProductCountDictionary[product.Key] = numberOfProductAdverts;
+        }
+
+        static async Task MakeCuesAsync(string emailContentLinks) // visual, audio, email
+        {
             Console.BackgroundColor = ConsoleColor.Red;
-            //audio cue
-            Console.Beep(470, 1000);
-            //email cue
-            SendEmail(emailContentLinks);
+            WindowsFunction.BringConsoleToFront();
+
+            new Thread(() => Console.Beep(470, 1000)).Start();
+
+            await SendMailAsync(emailContentLinks);
         }
 
         //extracts number of available offers from html page string
@@ -94,7 +114,7 @@ namespace SkelbiuLt_AlertAboutNewOffers
             return isNumber ? result : 0;
         }
 
-        static void SendEmail(string emailContentLinks)
+        static async Task SendMailAsync(string emailContentLinks)
         {
 
             var client = new SmtpClient("smtp.gmail.com", 587)
@@ -112,8 +132,7 @@ namespace SkelbiuLt_AlertAboutNewOffers
             mailMessage.Subject = "Naujas skelbimas";
             mailMessage.Body = emailContentLinks;
             mailMessage.IsBodyHtml = true;
-            client.Send(mailMessage);
+            await client.SendMailAsync(mailMessage);
         }
     }
 }
-
---
+++ SkelbiuLt_AlertAboutNewOffers/WindowsFunction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkelbiuLt_AlertAboutNewOffers
+{
+    //makes console get in focus (on top)
+    public static class WindowsFunction
+    {
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        public static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        public static void BringConsoleToFront()
+        {
+            SetForegroundWindow(GetConsoleWindow());
+        }
+    }
+}
